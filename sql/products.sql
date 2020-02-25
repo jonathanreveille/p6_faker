9ba47db9f651836ea11bc297e2152c33a2c6ee70
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2020-02-25 11:30:18 +0100

    minor changes

--- sql/products.sql
+++ sql/products.sql
@@ -14,3 +14,20 @@ INSERT INTO Product (name_product, category_idCategory) VALUES ("Opéra", "4");
 INSERT INTO Product (name_product, category_idCategory) VALUES ("Paris-Brest", "4");
 INSERT INTO Product (name_product, category_idCategory) VALUES ("Fôret-noir", "4");
 
+
+
+
+-- example 
+UPDATE `mydb`.`Product` SET `unit_price` = '10.00' WHERE (`idProduct` = '1') and (`Category_idCategory` = '1');
+UPDATE `mydb`.`Product` SET `unit_price` = '12.00' WHERE (`idProduct` = '2') and (`Category_idCategory` = '1');
+UPDATE `mydb`.`Product` SET `unit_price` = '12.00' WHERE (`idProduct` = '3') and (`Category_idCategory` = '2');
+UPDATE `mydb`.`Product` SET `unit_price` = '12.00' WHERE (`idProduct` = '4') and (`Category_idCategory` = '1');
+UPDATE `mydb`.`Product` SET `unit_price` = '12.00' WHERE (`idProduct` = '5') and (`Category_idCategory` = '1');
+UPDATE `mydb`.`Product` SET `unit_price` = '12.00' WHERE (`idProduct` = '6') and (`Category_idCategory` = '1');
+UPDATE `mydb`.`Product` SET `unit_price` = '2.00' WHERE (`idProduct` = '7') and (`Category_idCategory` = '3');
+UPDATE `mydb`.`Product` SET `unit_price` = '2.00' WHERE (`idProduct` = '8') and (`Category_idCategory` = '3');
+UPDATE `mydb`.`Product` SET `unit_price` = '2.00' WHERE (`idProduct` = '9') and (`Category_idCategory` = '3');
+UPDATE `mydb`.`Product` SET `unit_price` = '3.00' WHERE (`idProduct` = '10') and (`Category_idCategory` = '4');
+UPDATE `mydb`.`Product` SET `unit_price` = '3.00' WHERE (`idProduct` = '11') and (`Category_idCategory` = '4');
+UPDATE `mydb`.`Product` SET `unit_price` = '3.00' WHERE (`idProduct` = '12') and (`Category_idCategory` = '4');
+UPDATE `mydb`.`Product` SET `unit_price` = '3.00' WHERE (`idProduct` = '14') and (`Category_idCategory` = '4');
