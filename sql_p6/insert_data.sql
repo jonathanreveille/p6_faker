ccd2a51953b129b760a4ab9e5b0419c2b2abd593
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2020-02-26 13:17:27 +0100

    finished files for dump and create tables

--- sql_p6/insert_data.sql
+++ sql_p6/insert_data.sql
@@ -402,29 +402,49 @@ INSERT INTO status (status_name) VALUES("order_is_delivered");
 -- insert users (clients, employees, deliverers)
 use mydb;
 SELECT * FROM mydb.User;
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Véronique','Renaud','rene67@voila.fr','06 15 47 82 83','*TJClGFZI5', 1, '1');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Richard','Bourgeois','emmanuelleschmitt@ifrance.com','+33 (0)1 00 67 55 27','n$7%$55sGN', 1, '2');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Aimé','Begue','sloiseau@free.fr','0379373002','Mj66LbmD@j', 1, '3');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Clémence','Etienne','rroux@orange.fr','02 58 94 91 74','NtYc9Oh@f^', 1, '4');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Richard','Tanguy','uchevalier@launay.fr','+33 3 74 65 63 70','VP)5CIc#vS', 1, '5');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Nath','Chauvin','pgros@live.com','+33 5 86 57 94 27','_OC5BDYzc6', 1, '6');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Monique','Humbert','mariebernier@baudry.fr','08 07 13 65 50','(U4NupCln_', 1, '7');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Maude','Jackie','quenneni@laposte.fr','01 49 68 49 82','aaL8HdjJ%', 1, '8');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Véronique','Renaud','rene67@voila.fr','06 15 47 82 83','*TJClGFZI5', 1, '1');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Richard','Bourgeois','emmanuelleschmitt@ifrance.com','+33 (0)1 00 67 55 27','n$7%$55sGN', 1, '2');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Aimé','Begue','sloiseau@free.fr','0379373002','Mj66LbmD@j', 1, '3');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Clémence','Etienne','rroux@orange.fr','02 58 94 91 74','NtYc9Oh@f^', 1, '4');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Richard','Tanguy','uchevalier@launay.fr','+33 3 74 65 63 70','VP)5CIc#vS', 1, '5');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Nath','Chauvin','pgros@live.com','+33 5 86 57 94 27','_OC5BDYzc6', 1, '6');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Monique','Humbert','mariebernier@baudry.fr','08 07 13 65 50','(U4NupCln_', 1, '7');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Maude','Jackie','quenneni@laposte.fr','01 49 68 49 82','aaL8HdjJ%', 1, '8');
 
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Employee1','Cashier1','oc_pizza1@oc.fr','01 24 24 24 24','ahQsdkj', 4, '9');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Employee2','Cashier2','oc_pizza2@oc.fr','01 24 24 24 25','lkqjdqD', 4, '10');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Employee3','Cashier3','oc_pizza3@oc.fr','01 24 24 24 26','kqjnd)(', 4, '11');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Employee4','Cashier4','oc_pizza4@oc.fr','01 24 24 24 27','ksdmlk', 4, '12');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Employee5','Cashier5','oc_pizza5@oc.fr','01 24 24 24 28','WhAtOC', 4, '13');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Employee1','Cashier1','oc_pizza1@oc.fr','01 24 24 24 24','ahQsdkj', 4, '9');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Employee2','Cashier2','oc_pizza2@oc.fr','01 24 24 24 25','lkqjdqD', 4, '10');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Employee3','Cashier3','oc_pizza3@oc.fr','01 24 24 24 26','kqjnd)(', 4, '11');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Employee4','Cashier4','oc_pizza4@oc.fr','01 24 24 24 27','ksdmlk', 4, '12');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Employee5','Cashier5','oc_pizza5@oc.fr','01 24 24 24 28','WhAtOC', 4, '13');
 
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Admin1','SuperAdmin','admin_pizza5@oc.fr','01 48 49 48 49','seCreT', 2, '9');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Admin1','SuperAdmin','admin_pizza5@oc.fr','01 48 49 48 49','seCreT', 2, '9');
 
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Develivery1','Guy','delivery1@oc.fr','01 24 24 24 24','qdqsd', 3, '9');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Develivery2','Girl','delivery2@oc.fr','01 24 24 24 25','qdazed', 3, '10');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Develivery3','Guy','delivery2@oc.fr','01 24 24 24 25','kusqsd', 3, '10');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Develivery4','Guy','delivery3@oc.fr','01 24 24 24 26','pineaA', 3, '11');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Develivery5','Girl','delivery4@oc.fr','01 24 24 24 27','qdqsd', 3, '12');
-INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) VALUES ('Develivery6','Girl','delivery5@oc.fr','01 24 24 24 28','qdqsd', 3, '13');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Develivery1','Guy','delivery1@oc.fr','01 24 24 24 24','qdqsd', 3, '9');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Develivery2','Girl','delivery2@oc.fr','01 24 24 24 25','qdazed', 3, '10');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Develivery3','Guy','delivery2@oc.fr','01 24 24 24 25','kusqsd', 3, '10');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Develivery4','Guy','delivery3@oc.fr','01 24 24 24 26','pineaA', 3, '11');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Develivery5','Girl','delivery4@oc.fr','01 24 24 24 27','qdqsd', 3, '12');
+INSERT INTO user (name, surname, email, phone, password, Role_roleId, Adress_idAdress) 
+VALUES ('Develivery6','Girl','delivery5@oc.fr','01 24 24 24 28','qdqsd', 3, '13');
 
 -- insert orders
 SELECT * FROM mydb.Order;
@@ -504,3 +524,40 @@ INSERT INTO `mydb`.`OrderLine` (`quantity`, `unit_cost`, `Order_idOrder`, `Produ
 VALUES ('1', '12', '16', '4', '1');
 
 -- insert in db payment
+INSERT INTO `mydb`.`Payment`(`date_time_payment`,`amount_payment`,`type_payment`, `Order_idOrder`)
+VALUES(`2020-02-18 17:02:00`,`36.00`,`CREDIT CARD`,`6`);
+
+INSERT INTO `mydb`.`Payment`(`date_time_payment`,`amount_payment`,`type_payment`, `Order_idOrder`)
+VALUES(`2020-02-18 17:03:00`,`16.00`,`CREDIT CARD`,`7`);
+
+INSERT INTO `mydb`.`Payment`(`date_time_payment`,`amount_payment`,`type_payment`, `Order_idOrder`)
+VALUES(`2020-02-18 17:05:58`,`12.00`,`CREDIT CARD`,`8`);
+
+INSERT INTO `mydb`.`Payment`(`date_time_payment`,`amount_payment`,`type_payment`, `Order_idOrder`)
+VALUES(`2020-02-18 17:14:59`,`24.00`,`CREDIT CARD`,`9`);
+
+INSERT INTO `mydb`.`Payment`(`date_time_payment`,`amount_payment`,`type_payment`, `Order_idOrder`)
+VALUES(`2020-02-18 17:16:00`,`24.00`,`CREDIT CARD`,`10`);
+
+INSERT INTO `mydb`.`Payment`(`date_time_payment`,`amount_payment`,`type_payment`, `Order_idOrder`)
+VALUES(`2020-02-18 17:17:00`,`36.00`,`CASH`,`11`);
+
+INSERT INTO `mydb`.`Payment`(`date_time_payment`,`amount_payment`,`type_payment`, `Order_idOrder`)
+VALUES(`2020-02-19 14:30:00`,`40.00`,`CASH`,`12`);
+
+INSERT INTO `mydb`.`Payment`(`date_time_payment`,`amount_payment`,`type_payment`, `Order_idOrder`)
+VALUES(`2020-02-19 14:14:01`,`56.00`,`CREDIT CARD`,`15`);
+
+INSERT INTO `mydb`.`Payment`(`date_time_payment`,`amount_payment`,`type_payment`, `Order_idOrder`)
+VALUES(`2020-02-19 14:30:02`,`12.00`,`CREDIT CARD`,`16`);
+
+
+
+
+
+
+
+
+
+
+
